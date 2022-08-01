68322babe5a15722d815a5dcce3b7896b851d0dc
coderayush31 <98007341+coderayush31@users.noreply.github.com>
2022-08-01 18:53:33 +0530

    Add files via upload

---
+++ fibonnaeci.cpp
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int count,n1=0,n2=1,n,n3=1;
+
+    scanf("%d",&n);// n is the no. of terms 
+    printf("\n 0");
+for(count=0;count<n;count++)
+{
+    printf("\n %d",n3);
+n3=n1+n2;
+n1=n2;
+n2=n3;
+  
+} 
+
+return 0;
+    }
+
